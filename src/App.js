15d3e4df1e9df406d8c41f3b6599d6bacc7059c6
vvictorious <vvictorious30@gmail.com>
2024-05-01 13:58:10 -0700

    delete unused variabless

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./main.scss";
 import axios from "axios";
 
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
 import "../sylesheets/barChart.scss";
 
